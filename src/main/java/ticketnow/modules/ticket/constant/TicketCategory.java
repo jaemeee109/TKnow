905d6ea934bf295a1bc8fd46e4dbec52ac38b980
gomong0304 <gomong6765@gmail.com>
2025-11-03 01:18:38 +0900

    TKnow_ticket ahebf 1차 커밋

---
+++ src/main/java/ticketnow/modules/ticket/constant/TicketCategory.java
@@ -0,0 +1,6 @@
+package ticketnow.modules.ticket.constant;
+
+public enum TicketCategory {
+	CONCERT, MUSICAL, SPORTS, EXHIBITION
+	// 콘서트, 뮤지컬, 스포츠, 전시회
+}
---
+++ src/main/java/ticketnow/modules/ticket/constant/TicketStatus.java
@@ -0,0 +1,7 @@
+package ticketnow.modules.ticket.constant;
+
+public enum TicketStatus {
+	
+	ON_SALE, SOLD_OUT, SCHEDULED, CLOSED
+	// 핀메증 . 매진, 오픈예정, 판매종료
+}

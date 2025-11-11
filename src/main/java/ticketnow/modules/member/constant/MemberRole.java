85291f0413efb22f641ee00a5c09e95e0e77b0ea
hyejin-jun <h77200407@gmail.com>
2025-11-11 16:01:36 +0900

    Delete src/main directory

--- src/main/java/ticketnow/modules/member/constant/MemberRole.java
+++
@@ -1,6 +0,0 @@
-package ticketnow.modules.member.constant;
-
-public enum MemberRole {
-	ADMIN, USER, WITHDRAWN
-	// 관리자, 회원, 탈퇴계정
-}
--- src/main/java/ticketnow/modules/ticket/constant/SeatStatus.java
+++
@@ -1,6 +0,0 @@
-package ticketnow.modules.ticket.constant;
-
-public enum SeatStatus {
-	AVAILABLE, HOLD, RESERVED, PAID, CANCELED
-	// 예매가능, 임시홀드(선점), 예약완료, 결제완료, 예매취소
-}

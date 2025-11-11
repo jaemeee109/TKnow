acbbfa2fde28b7f863753030a217cad6a199113b
220-12\hyejin <h77200407@gmail.com>
2025-11-11 15:42:19 +0900

    Revert "2025.11.11 티켓나우 프론트 및 백엔드 수정"
    This reverts commit 897d5689b78d90e460f8d8e0712570b087cd8c92.

--- tknow-b/src/main/java/ticketnow/modules/board/constant/CategoryType.java
+++
@@ -1,6 +0,0 @@
-package ticketnow.modules.board.constant;
-
-public enum CategoryType {
-	SHOW_INFO, TICKET_BOOKING, PAYMENT, FREE, ACCOUNT, TECHNICAL, OTHER
-  // 공연정보문의, 예매문의 , 결제환불, 계정문의, 시스템문의, 기타문의
-}
--- tknow-b/src/main/java/ticketnow/modules/pay/constant/PayStatus.java
+++
@@ -1,6 +0,0 @@
-package ticketnow.modules.pay.constant;
-
-public enum PayStatus {
-	READY, APPROVED, CANCELED, PARTIAL_REFUND, FAILED
-	// 결제준비중, 결제승인완료, 결제취소완료, 부분환불완료, 결제실패
-}
